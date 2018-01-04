1ba0999c73b8c8ff06e75c34e3042f85d59a4496
Shawn Levin <shawnlevin22@gmail.com>
2018-01-03 19:31:48 -0800

    Added jquery clikc and animate

--- main.js
+++ main.js
@@ -8,8 +8,25 @@ function initializeGame() {
     ///Test//
     $("div[col=0]").click(function () {
         console.log('clicked');
-        $('.chipContainer1, .chip1').addClass("animateChip1");
+        var chip = $('<div>', {
+            class: 'chip'
+        });
+        $(".chipContainer1").append(chip);
+        var col = $("div[col=0]");
+        var colPosition = col.position();
+        console.log(( "left: " + colPosition.left + ", top: " + colPosition.top ));
+
+        $(".chip").animate({top:'1000px'});
     });
+        //dom create your falling chip element with appropriate class
+        //get the position of the column that was clicked
+
+        //place it at the top position of 1 chip above the column
+        //append new chip to column in question
+        //determine height of chip final resting position
+        //use animate function to move chip to new position
+        //find out how far down to animate chip
+        // $(this).parent().append(clone);
 
 
 }
@@ -33,26 +50,18 @@ function GameBoard() {
 
     this.generateBoard = function() {
         var rowArray = []
+
         var chipRow = $('<div>', {
             class: 'chipRow'
         });
         $('#container').append(chipRow);
 
-
         for(var chip1 = 1; chip1 < 7; chip1++) {
             var $chipcontainer = $('<div>', {
                 class: 'chipContainer' + chip1,
             });
-            var $chip1 = $('<div>', {
-                class: 'chip' + chip1,
-            });
             chipRow.append($chipcontainer);
-            for(var i = 1; i < 8; i++){
-                var $chip1 = $('<div>', {
-                    class: 'chip' + i,
-                });
-                $($chipcontainer).append($chip1);
-            }
+
         }
 
 
@@ -82,37 +91,37 @@ function GameBoard() {
 
     //Switch player 1 to player 2 after dropping chip
     // turn numbers equal to 42
-
-    this.placeChip = function(){
-        var row = $(this).attr('row');
-        var col = $(this).attr('col');
-
-        if(this.playing){
-            if(this.gameBoard[row][col] === 0){
-                if (this.playerTurn === 1){
-                    //$(this).placedChip player 1 picked chip in the cell function
-                    this.gameBoard[row][col] = 1;
-                } else {
-                    //$(this).placedChip player 1 picked chip in the cell function
-                    this.gameBoard[row][col] = 2;
-                }
-                //put function below that states if winner is found.
-                if(!winner()){
-                    if(this.playerTurn === 1){
-                        this.playerTurn = 2 ;
-                        ++this.turnNumber;
-                    } else {
-                        this.playerTurn =1;
-                        ++this.turnNumber;
-                    }
-                    //display area to put whos turn it is
-                    //$('....;).text("Player " + this.playerTurn +"'s turn");
-
-                }
-            }
-
-        }
-    }
+    //
+    // this.placeChip = function(){
+    //     var row = $(this).attr('row');
+    //     var col = $(this).attr('col');
+    //
+    //     if(this.playing){
+    //         if(this.gameBoard[row][col] === 0){
+    //             if (this.playerTurn === 1){
+    //                 //$(this).placedChip player 1 picked chip in the cell function
+    //                 this.gameBoard[row][col] = 1;
+    //             } else {
+    //                 //$(this).placedChip player 1 picked chip in the cell function
+    //                 this.gameBoard[row][col] = 2;
+    //             }
+    //             //put function below that states if winner is found.
+    //             if(!winner()){
+    //                 if(this.playerTurn === 1){
+    //                     this.playerTurn = 2 ;
+    //                     ++this.turnNumber;
+    //                 } else {
+    //                     this.playerTurn =1;
+    //                     ++this.turnNumber;
+    //                 }
+    //                 //display area to put whos turn it is
+    //                 //$('....;).text("Player " + this.playerTurn +"'s turn");
+    //
+    //             }
+    //         }
+    //
+    //     }
+    // }
 
     // this.winnerWinner = function(){
     //     //check for this
