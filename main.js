c7035ce9ee37f1dc4c9d304611cb44e19a449542
Shawn Levin <shawnlevin22@gmail.com>
2018-01-05 15:00:22 -0800

    Fixed columns and 1st check loop

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(initializeGame);
 var game = null;
 var currentPlayer = 0;
+//gameBoard[0].push[0]
 var gameBoard = [
     [],
     [],
@@ -9,6 +10,7 @@ var gameBoard = [
     [],
     [],
     []
+
 ];
 
 function initializeGame() {
@@ -19,8 +21,9 @@ function initializeGame() {
     $(".cell2").click(chipCreate2);
     $(".cell3").click(chipCreate3);
     $(".cell4").click(chipCreate4);
-    $(".cell5").click(chipCreate5);
-
+    $(".cell5").click(chipCreate5)
+    $(".cell6").click(chipCreate6);
+    togglePlayerHighlight();
     //intro
     $("#intro").hide().fadeIn(100);
     $("#play").on("click", function (e) {
@@ -31,7 +34,6 @@ function initializeGame() {
     $("#reset").on("click", function(){
         $("#container div").empty();
         gameBoard = [
-            [],
             [],
             [],
             [],
@@ -57,7 +59,7 @@ function GameBoard() {
         });
         $('#container').append(chipRow);
 
-        for (var col = 0; col < 6; col++) {
+        for (var col = 0; col < 7; col++) {
             var colNum = $('<div>', {
                 class: 'cell'+col,
             });
@@ -80,8 +82,7 @@ function GameBoard() {
 
 function togglePlayer() {
     currentPlayer = 1 - currentPlayer;
-
-
+    togglePlayerHighlight();
 
     // $("#player1").toggleClass("playerColor");
     // $("#player2").toggleClass("playerColor2");
@@ -100,10 +101,10 @@ function togglePlayerHighlight() {
 
     function chipCreate0() {
         var dropLevel = this.childElementCount;
-        if(dropLevel >= 7){
+        if(dropLevel >= 6){
             return;
         }
-        var dropPositions = ['81.6%','68%','54.2%','40.4%','26.6%','12.9%'];
+        var dropPositions = ['81.6%','66%','50.2%','34.4%','18.6%','2.9%'];
         var chip = $('<div>', {
             class: 'chip',
             class:'player' + currentPlayer
@@ -111,18 +112,19 @@ function togglePlayerHighlight() {
         $(".cell0").append(chip)
         $(chip).animate({'top': dropPositions[dropLevel]}, 2000);
         gameBoard[0].push(currentPlayer);
-        togglePlayerHighlight();
-        checkHorizon(gameBoard);
-        checkVertical(gameBoard);
         togglePlayer();
+        // checkHorizon(gameBoard);
+        checkVertical(gameBoard);
+        // checkRightUp(gameBoard);
+        // checkUpLeft(gameBoard);
     }
 
     function chipCreate1() {
         var dropLevel = this.childElementCount;
-        if(dropLevel >= 7){
+        if(dropLevel >= 6){
             return;
         }
-        var dropPositions = ['81.6%','68%','54.2%','40.4%','26.6%','12.9%'];
+        var dropPositions = ['81.6%','66%','50.2%','34.4%','18.6%','2.9%'];
         var chip = $('<div>', {
             class: 'chip',
             class:'player' + currentPlayer
@@ -130,18 +132,20 @@ function togglePlayerHighlight() {
         $(".cell1").append(chip)
         $(chip).animate({'top': dropPositions[dropLevel]}, 2000);
         gameBoard[1].push(currentPlayer);
-        togglePlayerHighlight();
-        checkHorizon(gameBoard);
-        checkVertical(gameBoard);
         togglePlayer();
+        // checkHorizon(gameBoard);
+        checkVertical(gameBoard);
+        // checkRightUp(gameBoard);
+        // checkUpLeft(gameBoard);
+
     }
 
     function chipCreate2() {
         var dropLevel = this.childElementCount;
-        if(dropLevel >= 7){
+        if(dropLevel >= 6){
             return;
         }
-        var dropPositions = ['81.6%','68%','54.2%','40.4%','26.6%','12.9%'];
+        var dropPositions = ['81.6%','66%','50.2%','34.4%','18.6%','2.9%'];
         var dropLevel = this.childElementCount;
         var chip = $('<div>', {
             class: 'chip',
@@ -150,18 +154,20 @@ function togglePlayerHighlight() {
         $(".cell2").append(chip);
         $(chip).animate({'top': dropPositions[dropLevel]}, 2000);
         gameBoard[2].push(currentPlayer);
-        togglePlayerHighlight();
-        checkHorizon(gameBoard);
-        checkVertical(gameBoard);
         togglePlayer();
+        // checkHorizon(gameBoard);
+        checkVertical(gameBoard);
+        // checkRightUp(gameBoard);
+        // checkUpLeft(gameBoard);
+
     }
 
     function chipCreate3() {
         var dropLevel = this.childElementCount;
-            if(dropLevel >= 7){
+            if(dropLevel >= 6){
             return;
             }
-        var dropPositions = ['81.6%','68%','54.2%','40.4%','26.6%','12.9%'];
+        var dropPositions = ['81.6%','66%','50.2%','34.4%','18.6%','2.9%'];
         var chip = $('<div>', {
             class: 'chip',
             class:'player' + currentPlayer
@@ -169,18 +175,19 @@ function togglePlayerHighlight() {
         $(".cell3").append(chip);
         $(chip).animate({'top': dropPositions[dropLevel]}, 2000);
         gameBoard[3].push(currentPlayer);
-
-        togglePlayerHighlight();
-        checkHorizon(gameBoard);
-        checkVertical(gameBoard);
         togglePlayer();
+        // checkHorizon(gameBoard);
+        checkVertical(gameBoard);
+        // checkRightUp(gameBoard);
+        // checkUpLeft(gameBoard);
+
     }
     function chipCreate4() {
         var dropLevel = this.childElementCount;
-        if(dropLevel >= 7){
+        if(dropLevel >= 6){
             return;
         }
-        var dropPositions = ['81.6%','68%','54.2%','40.4%','26.6%','12.9%'];
+        var dropPositions = ['81.6%','66%','50.2%','34.4%','18.6%','2.9%'];
         var chip = $('<div>', {
             class: 'chip',
             class:'player' + currentPlayer
@@ -188,18 +195,20 @@ function togglePlayerHighlight() {
         $(".cell4").append(chip);
         $(chip).animate({'top': dropPositions[dropLevel]}, 2000);
         gameBoard[4].push(currentPlayer);
-        togglePlayerHighlight();
-        checkHorizon(gameBoard);
-        checkVertical(gameBoard);
         togglePlayer();
+        // checkHorizon(gameBoard);
+        checkVertical(gameBoard);
+        // checkRightUp(gameBoard);
+        // checkUpLeft(gameBoard);
+
     }
 
     function chipCreate5() {
         var dropLevel = this.childElementCount;
-        if(dropLevel >= 7){
+        if(dropLevel >= 6){
             return;
         }
-        var dropPositions = ['81.6%','68%','54.2%','40.4%','26.6%','12.9%'];
+        var dropPositions = ['81.6%','66%','50.2%','34.4%','18.6%','2.9%'];
         var chip = $('<div>', {
             class: 'chip',
             class:'player' + currentPlayer
@@ -207,22 +216,43 @@ function togglePlayerHighlight() {
         $(".cell5").append(chip);
         $(chip).animate({'top': dropPositions[dropLevel]}, 2000);
         gameBoard[5].push(currentPlayer);
-        togglePlayerHighlight();
-        checkHorizon(gameBoard);
-        checkVertical(gameBoard);
         togglePlayer();
+        // checkHorizon(gameBoard);
+        checkVertical(gameBoard);
+        // checkRightUp(gameBoard);
+        // checkUpLeft(gameBoard);
+
     }
 
+function chipCreate6() {
+    var dropLevel = this.childElementCount;
+    if(dropLevel >= 6){
+        return;
+    }
+    var dropPositions = ['81.6%','66%','50.2%','34.4%','18.6%','2.9%'];
+    var chip = $('<div>', {
+        class: 'chip',
+        class:'player' + currentPlayer
+    });
+    $(".cell6").append(chip);
+    $(chip).animate({'top': dropPositions[dropLevel]}, 2000);
+    gameBoard[6].push(currentPlayer);
+    togglePlayer();
+    // checkHorizon(gameBoard);
+    checkVertical(gameBoard);
+    // checkRightUp(gameBoard);
+    // checkUpLeft(gameBoard);
 
+}
 
     function checkHorizon(arr) {
         var counter = 1;
         for (var horizontalPiece = 0; horizontalPiece < arr.length - 1; horizontalPiece++) {
+            if (arr[horizontalPiece + 1][0] == undefined) {
+                return;
+            }
             if (arr[horizontalPiece][0] === arr[horizontalPiece + 1][0]) {
-                if (arr[horizontalPiece + 1][0] == undefined) {
-                    return
-                }
-                console.log("I have found a match");
+                console.log("Horizontal: I have found a match");
                 counter++;
                 if (counter === 4) {
                     console.log("winner");
@@ -230,72 +260,69 @@ function togglePlayerHighlight() {
                 }
 
             }
-
+         counter--;
         }
     }
 
-
+///////////VERTICAL IS WORKING PROPERLY///////////////////////////////////
     function checkVertical(arr) {
         var counter = 1;
-        for (var verticalPiece = 0; verticalPiece < arr[1].length; verticalPiece++) {
-            if (arr[1][verticalPiece] === arr[1][verticalPiece + 1]) {
-                if (arr[verticalPiece + 1][0] == undefined) {
-                    return
-                }
-                console.log("I have found a match");
-                counter++
-                if (counter === 4) {
-                    console.log("winner");
-                    // gameWon();
+        for(var x = 0; x < arr.length; x++) {
+            counter = 1;
+            for (var verticalPiece = 0; verticalPiece < arr[x].length - 1; verticalPiece++) {
+                if (arr[x][verticalPiece] === arr[x][verticalPiece + 1]) {
+                    console.log("Vertical: "+ x +" I have found a match");
+                    counter++
+                    if (counter === 4) {
+                        console.log("winner is at Row " + x);
+                        // gameWon();
+                    }
                 }
             }
         }
     }
-    // function checkUpRight(arr) {
-    //     var counter = 1;
-    //     for (var col = 0; col < arr.length;) {
-    //         for (var row = 0; row < arr[col].length;) {
-    //             if (arr[col][row] == arr[col + 1][row + 1]) {
-    //                 counter++;
-    //                 col++;
-    //                 row++;
-    //             }
-    //         }
-    //     }
-    //
-    //
-    // }
-    //
-    //
-    // function checkUpLeft(arr) {
-    //     var counter = 1;
-    //     for (var col = 0; col < arr.length;) {
-    //         for (var row = 0; row < arr[col].length;) {
-    //             if (arr[col][row] == arr[col - 1][row + 1]) {
-    //                 counter++;
-    //                 col++;
-    //                 row++;
-    //             }
-    //         }
-    //     }
-    //
-    //
-    // }
+//////////////////////////CONSOLE ERROR CANT READ PROPERTY OF UNDEFINED//////////////////////
+    function checkUpLeft(arr) {
+        var counter = 1;
+        for (var col = 0; col < arr.length;) {
+            for (var row = 0; row < arr[col].length;) {
+                if (arr[col][row] == arr[col - 1][row + 1]) {
+                    counter++;
+                    col++;
+                    row++;
+                }
+            }
+        }
 
-// console.log(gameBoard);
-// console.log(checkHorizon(gameBoard));
-// console.log(checkVertical(gameBoard));
 
-// function checkRightUp(arr){
-//   var counter =1;
-//   for(var col=0; col<arr.length; col++){
-//     for(var row=0; row<arr[col]; row++ ){
-//       if(arr[col][row] === arr[col + 1][row +1]){
-//           console.log("I have a match");
-//         counter++;
-//       }
+    }
+
+///////////THIS RUNS BUT DOES NOT FIND A MATCH///////////////////////////////////
+
+function checkRightUp(arr){
+  var counter =1;
+  for(var col=0; col<arr.length; col++){
+    for(var row=0; row<arr[col]; row++ ){
+      if(arr[col][row] === arr[col + 1][row +1]){
+          console.log("RightUp: I have a match");
+        counter++;
+      }
+    }
+   }
+}
+////////////////INFINITE LOOP NEEDS TO BE FIXED//////////////////////
+// function checkUpRight(arr) {
+//     var counter = 1;
+//     for (var col = 0; col < arr.length;) {
+//         for (var row = 0; row < arr[col].length;) {
+//             if (arr[col][row] == arr[col + 1][row + 1]) {
+//                 counter++;
+//                 col++;
+//                 row++;
+//             }
+//         }
 //     }
-//    }
+//
+//
 // }
-// console.log(gameBoard);
-// console.log(checkRightUp(gameBoard));
+/////////////////////////////////////////////////////////////////////////
